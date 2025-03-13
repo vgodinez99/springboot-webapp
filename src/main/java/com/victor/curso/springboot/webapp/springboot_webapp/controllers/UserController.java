96b90629e9f1f1b5f64dd526608b4d25b2502431
VicAnto99 <vgodinez99_IDK@outlook.com>
2025-03-13 17:20:23 -0600

    Video 13 - Pasar datos a la vista
    I create the model/map to send the data to the view.

--- src/main/java/com/victor/curso/springboot/webapp/springboot_webapp/controllers/UserController.java
+++ src/main/java/com/victor/curso/springboot/webapp/springboot_webapp/controllers/UserController.java
@@ -1,13 +1,29 @@
 package com.victor.curso.springboot.webapp.springboot_webapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
     @GetMapping("/details")
-    public String details(){
+    public String details(Model model){
+
+        model.addAttribute("title", "Hello world");
+        model.addAttribute("name", "Victor");
+        model.addAttribute("lastname", "Godinez");
+
         return "details";
     }
+    //Same proccess but with map
+    /*@GetMapping("/details")
+    public String details(Map<String, Object> model){
+
+        model.put("title", "Hello world");
+        model.put("name", "Victor");
+        model.put("lastname", "Godinez");
+
+        return "details";
+    }*/
 }
