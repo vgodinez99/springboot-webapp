f3df32e32066498f6c5e94b1476a6c9f03b88632
VicAnto99 <vgodinez99_IDK@outlook.com>
2025-03-24 17:02:00 -0600

    @RequestMapping use this to get new URL

--- src/main/java/com/victor/curso/springboot/webapp/springboot_webapp/controllers/UserRestController.java
+++ src/main/java/com/victor/curso/springboot/webapp/springboot_webapp/controllers/UserRestController.java
@@ -3,14 +3,15 @@ package com.victor.curso.springboot.webapp.springboot_webapp.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class UserRestController {
 
-    @GetMapping("/restDetails")
+    @GetMapping("/details")
     public Map<String, Object> restDetails(){
 
         Map<String, Object> body = new HashMap<>();
