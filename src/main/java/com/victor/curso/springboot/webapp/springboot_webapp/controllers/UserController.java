3fe2526e04744846b35e903f4626564b7b7bb721
VicAnto99 <vgodinez99_IDK@outlook.com>
2025-03-24 18:13:16 -0600

    Object Model - Video 16

--- src/main/java/com/victor/curso/springboot/webapp/springboot_webapp/controllers/UserController.java
+++ src/main/java/com/victor/curso/springboot/webapp/springboot_webapp/controllers/UserController.java
@@ -4,15 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.victor.curso.springboot.webapp.springboot_webapp.models.User;
+
 @Controller
 public class UserController {
 
     @GetMapping("/details")
     public String details(Model model){
 
+        User user = new User("Victor", "Godinez");
+
         model.addAttribute("title", "Hello world");
+        model.addAttribute("user", user);
+        
+        /* 
         model.addAttribute("name", "Victor");
         model.addAttribute("lastname", "Godinez");
+        */
 
         return "details";
     }
--- src/main/java/com/victor/curso/springboot/webapp/springboot_webapp/controllers/UserRestController.java
+++ src/main/java/com/victor/curso/springboot/webapp/springboot_webapp/controllers/UserRestController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.victor.curso.springboot.webapp.springboot_webapp.models.User;
+
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
@@ -14,11 +16,13 @@ public class UserRestController {
     @GetMapping("/details")
     public Map<String, Object> restDetails(){
 
+        User user = new User("Victor", "Godinez");
+
         Map<String, Object> body = new HashMap<>();
 
         body.put("title", "Hello world");
-        body.put("name", "Victor");
-        body.put("lastname", "Godinez"); 
+        body.put("User", user);
+        //body.put("Name", user.getName());
 
         return body;
     }
